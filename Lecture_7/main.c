0ae58f373f73d57f5b3c4f8bd8ae09f541344734
ibrahim <ibrahimalattar19@gmail.com>
2026-07-13 18:33:08 +0300

    Update lecture 7 main code

--- Lecture_7/main.c
+++ Lecture_7/main.c
@@ -1,15 +1,20 @@
 #include <stdio.h>
 #include "defines.h"
 
-/* General definitions */
+
 const u8 MAX_SPEED = 255;
 const u32 DEVICE_ID = 987654321;
 
 void PrintSensorValue(const u16 *ptr);
+
 void PrintSensorValue(const u16 *ptr)
 {
+    // I cannot change the value using ptr
+    // *ptr = 0;
+
     printf("The sensor temperature is: %u\n", *ptr);
 }
+
 int main()
 {
     u16 temperature = 75;
@@ -18,6 +23,9 @@ int main()
     printf("System Booting...\n");
     printf("Max Speed Configuration: %u\n", MAX_SPEED);
 
+    // I cannot change MAX_SPEED because it is const
+    // MAX_SPEED = 200;
+
     PrintSensorValue(&temperature);
 
     u8 *const hardware_ptr = &PORTA_DATA;
@@ -25,12 +33,20 @@ int main()
     *hardware_ptr = 100;
     *hardware_ptr = 255;
 
+    // I cannot change the address of hardware_ptr
+    // hardware_ptr = &temperature;
+
     printf("Port data is: %u\n", *hardware_ptr);
 
     const u32 *const id_ptr = &DEVICE_ID;
+
+    // I cannot change the value using id_ptr
+    // *id_ptr = 123;
+
+    // I cannot change the address of id_ptr
+    // id_ptr = &another_variable;
 
     printf("Device ID is: %u\n", *id_ptr);
 
     return 0;
 }
-
